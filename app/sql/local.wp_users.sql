00bd30cc46aa528ce75d6a080a32e2273f15c5bc
Will Oakley <woakley5@icloud.com>
2019-07-02 20:35:52 -0700

    divi site

--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"mdbadmin","$P$B0Ty616a5KavmQ0yv/Y9eJ6KDA8SSY1","mdbadmin","dev-email@flywheel.local","","2019-04-07 00:14:22","",0,"mdbadmin");
