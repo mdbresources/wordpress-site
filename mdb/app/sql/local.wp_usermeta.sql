743761f22d90080d0cba73cded846b7fc8ae4f4c
Will Oakley <woakley5@icloud.com>
2019-07-14 21:13:40 -0700

    hi

---
+++ mdb/app/sql/local.wp_usermeta.sql
@@ -0,0 +1,24 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","woakley5"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"syntax_highlighting","true"),
+(7,1,"comment_shortcuts","false"),
+(8,1,"admin_color","fresh"),
+(9,1,"use_ssl","0"),
+(10,1,"show_admin_bar_front","true"),
+(11,1,"locale",""),
+(12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(13,1,"wp_user_level","10"),
+(14,1,"dismissed_wp_pointers",""),
+(15,1,"show_welcome_panel","1"),
+(16,1,"session_tokens","a:2:{s:64:\"b1c34a02cadd052d30de2065f44f12aeddd73b5a51c3b93512a10681034aed61\";a:4:{s:10:\"expiration\";i:1562370239;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:117:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15\";s:5:\"login\";i:1562197439;}s:64:\"b3ee0db68586250c0243a59b42050697375f8bfde8a5c1354c72b7b29488df85\";a:4:{s:10:\"expiration\";i:1562370979;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36\";s:5:\"login\";i:1562198179;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","4"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"wp_user-settings","libraryContent=browse"),
+(20,1,"wp_user-settings-time","1562200052");
---
+++ mdb/app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"woakley5","$P$BQfkF4oj4.6bjnH8LMhux9dVry5xdT0","woakley5","dev-email@flywheel.local","","2019-07-03 23:43:28","",0,"woakley5");
